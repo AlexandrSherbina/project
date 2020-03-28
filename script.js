039a2cbdd46b9d9dad05fc6405d7463809f4590a
AlexandrSherbina <example@gmail.com>
2020-03-28 11:02:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,148 @@
+'use strict';
+
+a = 5;
+
+console.log(a);
+
+/*let myName = 'Alex';
+
+let bool = false;
+
+let modal;
+  console.log(myName);
+
+
+  console.log(-4/0);
+  console.log('string' * 9);
+
+  console.log('4' + 4);
+
+  let x = 5;
+ // x++;
+ x--;
+console.log(x);*/
+
+//alert(1);
+//let answer = confirm('ВАм  есть 18 лет?');
+//console.log(answer);
+
+//let answer = prompt('ВАм  есть 18 лет?', '');
+//console.log(answer);
+
+// let answers = [],
+//     questions = [
+//       'Как ваше имя?',
+//       'Как ваша фамилия?',
+//       'Сколько Вам лет?'
+//     ];
+// //3-й способ цикла
+//     let i = 0;
+//     while (i < questions.length) {
+//         answers[i] = prompt(questions[i], '');
+//         console.log(answers); //0
+//         i++; // 10
+//     }    
+
+//   document.write(answers);
+
+//2-й способ цикла
+// let i = 0;
+//     do {
+//       console.log(answers);
+//       answers[i] = prompt(questions[i], '');     
+//       i++;  
+//   } while (i < questions.length);
+   
+//   document.write(answers);
+  
+
+//циклы  1-й способ
+// for (let i = 0; i < questions.length; i++) {
+//     answers[i] = prompt(questions[i], '');
+// }
+
+// console.log(answers);
+
+
+// let i = 0;
+// do  {
+//     console.log(i);
+//     i++;  
+// } while (i < 10);
+
+
+// let i = 0;
+// while (i < 10) {
+//     console.log(i); //0
+//     i++; // 10
+// }
+
+
+//условия 
+
+// let age = 14;
+
+// if (age < 18) {
+//     alert('Вы можете войти');    
+// }
+
+
+// var leftBorderWidth = 1;
+// {
+//   let second = 2;
+// }
+
+// const pi = 3.14;
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone['name']);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+//alert("Hello world!");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+//let answer = +prompt("Есть ли вам  18?", "Да");
+
+ console.log(typeof(answer));
+
+ //конкатенация 
+ console.log('arr'+ '- object');
+ console.log(4 + +'- object');
+
+ //инкремент дикремент
+
+ let incr = 10,
+     decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log('2' === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
+
+
